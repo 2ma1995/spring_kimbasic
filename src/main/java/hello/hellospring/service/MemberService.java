5f55597505a55f931035feb8d60d0ea22710f852
2ma1995 <vthquddbsv@gmail.com>
2025-06-03 20:41:36 +0900

    회원서비스 테스트

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,8 +9,13 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-// 회원가입
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원가입
     public Long join(Member member) {
 //        같은 이름이 있는 중복 회원x
         validateDupulicateMember(member);//중복회원 검증
@@ -30,7 +35,7 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    public Optional<Member> findMember(Long memberId){
+    public Optional<Member> findOne(Long memberId){
         return memberRepository.findById(memberId);
     }
 }
--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -54,5 +54,6 @@ class MemoryMemberRepositoryTest {
          List<Member> result = repository.findAll();
          assertThat(result.size()).isEqualTo(2);
 
+
     }
  }
